3262225cbb81505cc3c121a172bd55332e7079b7
BushTrain460615 <72326501+BushTrain460615@users.noreply.github.com>
2022-08-30 11:09:36 -0500

    updated title state

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -290,7 +290,9 @@ class TitleState extends MusicBeatState
 		logoBl.animation.addByPrefix('bump', 'logo bumpin', 24, false);
 		logoBl.animation.play('bump');
 		logoBl.updateHitbox();
-		logoBl.screenCenter();
+		logoBl.x = -10;
+		logoBl.y = 10;
+		//logoBl.screenCenter();
 		// logoBl.color = FlxColor.BLACK;
 
 		swagShader = new ColorSwap();
